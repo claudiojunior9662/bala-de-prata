4d588e911e555fde3aad1d10a42771e069cbdbcf
1113778771 <1113778771@GRA22.eggcf.tux>
2021-05-03 17:28:36 -0300

    Atualização 2.5.1 1

---
+++ src/module-info.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+module BalaDePrata20 {
+    requires itext;
+    requires itextpdf;
+    requires com.fasterxml.jackson.databind;
+    requires jcalendar;
+    requires jsch;
+    requires json;
+    requires AbsoluteLayout;
+    requires mysql.connector.java;
+}
